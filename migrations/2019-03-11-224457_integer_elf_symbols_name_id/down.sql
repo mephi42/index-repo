dc73423560db3f5f278af4231e5fa2e991c7a6d4
mephi42 <mephi42@gmail.com>
2019-03-11 23:53:59 +0100

    Fix type of elf_symbols.name_id

---
+++ migrations/2019-03-11-224457_integer_elf_symbols_name_id/down.sql
@@ -0,0 +1,17 @@
+DROP INDEX elf_symbols_name_id_index;
+CREATE TABLE elf_symbols_tmp
+(
+  id       INTEGER NOT NULL PRIMARY KEY,
+  file_id  INTEGER NOT NULL,
+  name_id  VARCHAR NOT NULL,
+  st_info  INTEGER NOT NULL,
+  st_other INTEGER NOT NULL,
+  FOREIGN KEY (file_id) REFERENCES files (id),
+  FOREIGN KEY (name_id) REFERENCES strings (id)
+);
+INSERT INTO elf_symbols_tmp
+SELECT id, file_id, name_id, st_info, st_other
+FROM elf_symbols;
+DROP TABLE elf_symbols;
+ALTER TABLE elf_symbols_tmp
+  RENAME TO elf_symbols;
